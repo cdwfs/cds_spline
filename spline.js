1dc00734809cc2280379769d31dea21be6d9735c
Cort <github@dangerware.org>
2015-11-29 20:01:48 -0800

    Splines work with 1D-4D input points. Internally, 4D is used everywhere; missing components are assumed to be zero.

--- spline.js
+++ spline.js
@@ -28,23 +28,27 @@ var SPLINE = function() {
             var p1 = controlPoints[iSeg+1];
             var p2 = controlPoints[iSeg+2];
             var p3 = controlPoints[iSeg+3];
+            p0 = {x: p0.x || 0, y: p0.y || 0, z: p0.z || 0, w: p0.w || 0};
+            p1 = {x: p1.x || 0, y: p1.y || 0, z: p1.z || 0, w: p1.w || 0};
+            p2 = {x: p2.x || 0, y: p2.y || 0, z: p2.z || 0, w: p2.w || 0};
+            p3 = {x: p3.x || 0, y: p3.y || 0, z: p3.z || 0, w: p3.w || 0};
             var m = {
                 m00: p1.x,
                 m01: p1.y,
-                m02: 0,
-                m03: 0,
+                m02: p1.z,
+                m03: p1.w,
                 m10: (-tau)*p0.x + (tau)*p2.x,
                 m11: (-tau)*p0.y + (tau)*p2.y,
-                m12: 0,
-                m13: 0,
+                m12: (-tau)*p0.z + (tau)*p2.z,
+                m13: (-tau)*p0.w + (tau)*p2.w,
                 m20: (2*tau)*p0.x + (tau-3)*p1.x + (3-2*tau)*p2.x + (-tau)*p3.x,
                 m21: (2*tau)*p0.y + (tau-3)*p1.y + (3-2*tau)*p2.y + (-tau)*p3.y,
-                m22: 0,
-                m23: 0,
+                m22: (2*tau)*p0.z + (tau-3)*p1.z + (3-2*tau)*p2.z + (-tau)*p3.z,
+                m23: (2*tau)*p0.w + (tau-3)*p1.w + (3-2*tau)*p2.w + (-tau)*p3.w,
                 m30: (-tau)*p0.x + (2-tau)*p1.x + (tau-2)*p2.x + (tau)*p3.x,
                 m31: (-tau)*p0.y + (2-tau)*p1.y + (tau-2)*p2.y + (tau)*p3.y,
-                m32: 0,
-                m33: 0
+                m32: (-tau)*p0.z + (2-tau)*p1.z + (tau-2)*p2.z + (tau)*p3.z,
+                m33: (-tau)*p0.w + (2-tau)*p1.w + (tau-2)*p2.w + (tau)*p3.w,
             };
             segs.push({
                 geomMatrix: m,
@@ -61,7 +65,9 @@ var SPLINE = function() {
             var m = segs[uInt].geomMatrix;
             return {
                 x: ((m.m30*uFrac + m.m20)*uFrac + m.m10)*uFrac + m.m00,
-                y: ((m.m31*uFrac + m.m21)*uFrac + m.m11)*uFrac + m.m01
+                y: ((m.m31*uFrac + m.m21)*uFrac + m.m11)*uFrac + m.m01,
+                z: ((m.m32*uFrac + m.m22)*uFrac + m.m12)*uFrac + m.m02,
+                w: ((m.m33*uFrac + m.m23)*uFrac + m.m13)*uFrac + m.m03
             };
         }
 
@@ -75,7 +81,9 @@ var SPLINE = function() {
             var m = segs[uInt].geomMatrix;
             return {
                 x: (3*m.m30*uFrac + 2*m.m20)*uFrac + m.m10,
-                y: (3*m.m31*uFrac + 2*m.m21)*uFrac + m.m11
+                y: (3*m.m31*uFrac + 2*m.m21)*uFrac + m.m11,
+                z: (3*m.m32*uFrac + 2*m.m22)*uFrac + m.m12,
+                w: (3*m.m33*uFrac + 2*m.m23)*uFrac + m.m13,
             };
         }
         
@@ -94,23 +102,27 @@ var SPLINE = function() {
         for(iSeg=0; iSeg<controlPointsAndTangents.length-1; iSeg+=1) {
             var p0 = controlPointsAndTangents[iSeg+0];
             var p1 = controlPointsAndTangents[iSeg+1];
+            p0 = {  x: p0.x  || 0,  y: p0.y  || 0,  z: p0.z  || 0,  w: p0.w || 0,
+                   tx: p0.tx || 0, ty: p0.ty || 0, tz: p0.tz || 0, tw: p0.tw || 0 };
+            p1 = {  x: p1.x  || 0,  y: p1.y  || 0,  z: p1.z  || 0,  w: p1.w || 0,
+                   tx: p1.tx || 0, ty: p1.ty || 0, tz: p1.tz || 0, tw: p1.tw || 0 };
             var m = {
                 m00: p0.x,
                 m01: p0.y,
-                m02: 0,
-                m03: 0,
+                m02: p0.z,
+                m03: p0.w,
                 m10: p0.tx,
                 m11: p0.ty,
-                m12: 0,
-                m13: 0,
+                m12: p0.tz,
+                m13: p0.tw,
                 m20: (-3)*p0.x + (3)*p1.x + (-2)*p0.tx + (-1)*p1.tx,
                 m21: (-3)*p0.y + (3)*p1.y + (-2)*p0.ty + (-1)*p1.ty,
-                m22: 0,
-                m23: 0,
+                m22: (-3)*p0.z + (3)*p1.z + (-2)*p0.tz + (-1)*p1.tz,
+                m23: (-3)*p0.w + (3)*p1.w + (-2)*p0.tw + (-1)*p1.tw,
                 m30: (2)*p0.x + (-2)*p1.x + p0.tx + p1.tx,
                 m31: (2)*p0.y + (-2)*p1.y + p0.ty + p1.ty,
-                m32: 0,
-                m33: 0
+                m32: (2)*p0.z + (-2)*p1.z + p0.tz + p1.tz,
+                m33: (2)*p0.w + (-2)*p1.w + p0.tw + p1.tw
             };
             segs.push({
                 geomMatrix: m,
@@ -127,7 +139,9 @@ var SPLINE = function() {
             var m = segs[uInt].geomMatrix;
             return {
                 x: ((m.m30*uFrac + m.m20)*uFrac + m.m10)*uFrac + m.m00,
-                y: ((m.m31*uFrac + m.m21)*uFrac + m.m11)*uFrac + m.m01
+                y: ((m.m31*uFrac + m.m21)*uFrac + m.m11)*uFrac + m.m01,
+                z: ((m.m32*uFrac + m.m22)*uFrac + m.m12)*uFrac + m.m02,
+                w: ((m.m33*uFrac + m.m23)*uFrac + m.m13)*uFrac + m.m03,
             };
         }
 
@@ -141,7 +155,9 @@ var SPLINE = function() {
             var m = segs[uInt].geomMatrix;
             return {
                 x: (3*m.m30*uFrac + 2*m.m20)*uFrac + m.m10,
-                y: (3*m.m31*uFrac + 2*m.m21)*uFrac + m.m11
+                y: (3*m.m31*uFrac + 2*m.m21)*uFrac + m.m11,
+                z: (3*m.m32*uFrac + 2*m.m22)*uFrac + m.m12,
+                w: (3*m.m33*uFrac + 2*m.m23)*uFrac + m.m13,
             };
         }
 
@@ -161,35 +177,43 @@ var SPLINE = function() {
             var p1 = {
                 x:controlPointsAndTangents[iSeg+0].x + controlPointsAndTangents[iSeg+0].tx,
                 y:controlPointsAndTangents[iSeg+0].y + controlPointsAndTangents[iSeg+0].ty,
+                z:controlPointsAndTangents[iSeg+0].z + controlPointsAndTangents[iSeg+0].tz,
+                w:controlPointsAndTangents[iSeg+0].w + controlPointsAndTangents[iSeg+0].tw,
             };
             var p2 = {
                 x:controlPointsAndTangents[iSeg+1].x - controlPointsAndTangents[iSeg+1].tx,
                 y:controlPointsAndTangents[iSeg+1].y - controlPointsAndTangents[iSeg+1].ty,
+                z:controlPointsAndTangents[iSeg+1].z - controlPointsAndTangents[iSeg+1].tz,
+                w:controlPointsAndTangents[iSeg+1].w - controlPointsAndTangents[iSeg+1].tw,
             };
             var p3 = controlPointsAndTangents[iSeg+1];
+            p0 = {x: p0.x || 0, y: p0.y || 0, z: p0.z || 0, w: p0.w || 0};
+            p1 = {x: p1.x || 0, y: p1.y || 0, z: p1.z || 0, w: p1.w || 0};
+            p2 = {x: p2.x || 0, y: p2.y || 0, z: p2.z || 0, w: p2.w || 0};
+            p3 = {x: p3.x || 0, y: p3.y || 0, z: p3.z || 0, w: p3.w || 0};
             var m = {
                 m00: p0.x,
                 m01: p0.y,
-                m02: 0,
-                m03: 0,
+                m02: p0.z,
+                m03: p0.w,
                 m10: (-3)*p0.x + (3*p1.x),
                 m11: (-3)*p0.y + (3*p1.y),
-                m12: 0,
-                m13: 0,
+                m12: (-3)*p0.z + (3*p1.z),
+                m13: (-3)*p0.w + (3*p1.w),
                 m20: (3)*p0.x + (-6)*p1.x + (3)*p2.x,
                 m21: (3)*p0.y + (-6)*p1.y + (3)*p2.y,
-                m22: 0,
-                m23: 0,
+                m22: (3)*p0.z + (-6)*p1.z + (3)*p2.z,
+                m23: (3)*p0.w + (-6)*p1.w + (3)*p2.w,
                 m30: (-1)*p0.x + (3)*p1.x + (-3)*p2.x + p3.x,
                 m31: (-1)*p0.y + (3)*p1.y + (-3)*p2.y + p3.y,
-                m32: 0,
-                m33: 0
+                m32: (-1)*p0.z + (3)*p1.w + (-3)*p2.z + p3.z,
+                m33: (-1)*p0.w + (3)*p1.w + (-3)*p2.w + p3.w,
             };
             segs.push({
                 geomMatrix: m,
             });
         }
-        
+      
         function eval1(u) {
             var uClamp = clamp(u, 0, segs.length);
             var uInt = uClamp | 0, uFrac = uClamp - uInt;
@@ -200,7 +224,9 @@ var SPLINE = function() {
             var m = segs[uInt].geomMatrix;
             return {
                 x: ((m.m30*uFrac + m.m20)*uFrac + m.m10)*uFrac + m.m00,
-                y: ((m.m31*uFrac + m.m21)*uFrac + m.m11)*uFrac + m.m01
+                y: ((m.m31*uFrac + m.m21)*uFrac + m.m11)*uFrac + m.m01,
+                z: ((m.m32*uFrac + m.m22)*uFrac + m.m12)*uFrac + m.m02,
+                w: ((m.m33*uFrac + m.m23)*uFrac + m.m13)*uFrac + m.m03,
             };
         }
 
@@ -214,7 +240,9 @@ var SPLINE = function() {
             var m = segs[uInt].geomMatrix;
             return {
                 x: (3*m.m30*uFrac + 2*m.m20)*uFrac + m.m10,
-                y: (3*m.m31*uFrac + 2*m.m21)*uFrac + m.m11
+                y: (3*m.m31*uFrac + 2*m.m21)*uFrac + m.m11,
+                z: (3*m.m32*uFrac + 2*m.m22)*uFrac + m.m12,
+                w: (3*m.m33*uFrac + 2*m.m23)*uFrac + m.m13,
             };
         }
 
@@ -234,10 +262,26 @@ var SPLINE = function() {
             var p1 = controlPoints[iSeg+1];
             var p2 = controlPoints[iSeg+2];
             var p3 = controlPoints[iSeg+3];
+            p0 = {x: p0.x || 0, y: p0.y || 0, z: p0.z || 0, w: p0.w || 0};
+            p1 = {x: p1.x || 0, y: p1.y || 0, z: p1.z || 0, w: p1.w || 0};
+            p2 = {x: p2.x || 0, y: p2.y || 0, z: p2.z || 0, w: p2.w || 0};
+            p3 = {x: p3.x || 0, y: p3.y || 0, z: p3.z || 0, w: p3.w || 0};
             var t0 = 0;
-            var t1 = t0 + Math.pow( (p1.x-p0.x)*(p1.x-p0.x) + (p1.y-p0.y)*(p1.y-p0.y), alpha*0.5 );
-            var t2 = t1 + Math.pow( (p2.x-p1.x)*(p2.x-p1.x) + (p2.y-p1.y)*(p2.y-p1.y), alpha*0.5 );
-            var t3 = t2 + Math.pow( (p3.x-p2.x)*(p3.x-p2.x) + (p3.y-p2.y)*(p3.y-p2.y), alpha*0.5 );
+            var t1 = t0 + Math.pow( (p1.x-p0.x)*(p1.x-p0.x) +
+                                    (p1.y-p0.y)*(p1.y-p0.y) +
+                                    (p1.z-p0.z)*(p1.z-p0.z) +
+                                    (p1.w-p0.w)*(p1.w-p0.w),
+                                    alpha*0.5 );
+            var t2 = t1 + Math.pow( (p2.x-p1.x)*(p2.x-p1.x) +
+                                    (p2.y-p1.y)*(p2.y-p1.y) +
+                                    (p2.z-p1.z)*(p2.z-p1.z) +
+                                    (p2.w-p1.w)*(p2.w-p1.w),
+                                    alpha*0.5 );
+            var t3 = t2 + Math.pow( (p3.x-p2.x)*(p3.x-p2.x) +
+                                    (p3.y-p2.y)*(p3.y-p2.y) +
+                                    (p3.z-p2.z)*(p3.z-p2.z) +
+                                    (p3.w-p2.w)*(p3.w-p2.w),
+                                    alpha*0.5 );
 
             // Simplification: reduce centripetal Catmull-Rom to Hermite, as described in
             // https://stackoverflow.com/questions/9489736/catmull-rom-curve-with-no-cusps-and-no-self-intersections/23980479#23980479
@@ -248,33 +292,41 @@ var SPLINE = function() {
             // P1 to P2, the tangents must be scaled by (t2-t1) to put them in the appropriate range.
             var tan1 = {
                 x: (p1.x-p0.x)/(t1-t0) - (p2.x-p0.x)/(t2-t0) + (p2.x-p1.x)/(t2-t1),
-                y: (p1.y-p0.y)/(t1-t0) - (p2.y-p0.y)/(t2-t0) + (p2.y-p1.y)/(t2-t1)
+                y: (p1.y-p0.y)/(t1-t0) - (p2.y-p0.y)/(t2-t0) + (p2.y-p1.y)/(t2-t1),
+                z: (p1.z-p0.z)/(t1-t0) - (p2.z-p0.z)/(t2-t0) + (p2.z-p1.z)/(t2-t1),
+                w: (p1.w-p0.w)/(t1-t0) - (p2.w-p0.w)/(t2-t0) + (p2.w-p1.w)/(t2-t1),
             };
             var tan2 = {
                 x: (p2.x-p1.x)/(t2-t1) - (p3.x-p1.x)/(t3-t1) + (p3.x-p2.x) / (t3-t2),
-                y: (p2.y-p1.y)/(t2-t1) - (p3.y-p1.y)/(t3-t1) + (p3.y-p2.y) / (t3-t2)
+                y: (p2.y-p1.y)/(t2-t1) - (p3.y-p1.y)/(t3-t1) + (p3.y-p2.y) / (t3-t2),
+                z: (p2.z-p1.z)/(t2-t1) - (p3.z-p1.z)/(t3-t1) + (p3.z-p2.z) / (t3-t2),
+                w: (p2.w-p1.w)/(t2-t1) - (p3.w-p1.w)/(t3-t1) + (p3.w-p2.w) / (t3-t2),
             };
             tan1.x *= (t2-t1);
             tan1.y *= (t2-t1);
+            tan1.z *= (t2-t1);
+            tan1.w *= (t2-t1);
             tan2.x *= (t2-t1);
             tan2.y *= (t2-t1);
+            tan2.z *= (t2-t1);
+            tan2.w *= (t2-t1);
             var m = {
                 m00: p1.x,
                 m01: p1.y,
-                m02: 0,
-                m03: 0,
+                m02: p1.z,
+                m03: p1.w,
                 m10: tan1.x,
                 m11: tan1.y,
-                m12: 0,
-                m13: 0,
+                m12: tan1.z,
+                m13: tan1.w,
                 m20: (-3)*p1.x + (3)*p2.x + (-2)*tan1.x + (-1)*tan2.x,
                 m21: (-3)*p1.y + (3)*p2.y + (-2)*tan1.y + (-1)*tan2.y,
-                m22: 0,
-                m23: 0,
+                m22: (-3)*p1.z + (3)*p2.z + (-2)*tan1.z + (-1)*tan2.z,
+                m23: (-3)*p1.w + (3)*p2.w + (-2)*tan1.w + (-1)*tan2.w,
                 m30: (2)*p1.x + (-2)*p2.x + tan1.x + tan2.x,
                 m31: (2)*p1.y + (-2)*p2.y + tan1.y + tan2.y,
-                m32: 0,
-                m33: 0
+                m32: (2)*p1.z + (-2)*p2.z + tan1.z + tan2.z,
+                m33: (2)*p1.w + (-2)*p2.w + tan1.w + tan2.w,
             };
 
             segs.push({
@@ -292,7 +344,9 @@ var SPLINE = function() {
             var m = segs[uInt].geomMatrix;
             return {
                 x: ((m.m30*uFrac + m.m20)*uFrac + m.m10)*uFrac + m.m00,
-                y: ((m.m31*uFrac + m.m21)*uFrac + m.m11)*uFrac + m.m01
+                y: ((m.m31*uFrac + m.m21)*uFrac + m.m11)*uFrac + m.m01,
+                z: ((m.m32*uFrac + m.m22)*uFrac + m.m12)*uFrac + m.m02,
+                w: ((m.m33*uFrac + m.m23)*uFrac + m.m13)*uFrac + m.m03,
             };
         }
 
@@ -306,7 +360,9 @@ var SPLINE = function() {
             var m = segs[uInt].geomMatrix;
             return {
                 x: (3*m.m30*uFrac + 2*m.m20)*uFrac + m.m10,
-                y: (3*m.m31*uFrac + 2*m.m21)*uFrac + m.m11
+                y: (3*m.m31*uFrac + 2*m.m21)*uFrac + m.m11,
+                z: (3*m.m32*uFrac + 2*m.m22)*uFrac + m.m12,
+                w: (3*m.m33*uFrac + 2*m.m23)*uFrac + m.m13,
             };
         }
 
